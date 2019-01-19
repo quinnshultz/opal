7ba586c6a8ba172ec0072b0306e5226d76cfeda0
Quinn Shultz <vanoxite@gmail.com>
2019-01-19 12:28:24 -0700

    Updated sql procedures to match PasswordAccount

--- src/main/sql/initTablesAndProcs.sql
+++ src/main/sql/initTablesAndProcs.sql
@@ -26,22 +26,22 @@
 CREATE table opalUsers(ID int auto_increment
 , username varchar(256) UNIQUE NOT NULL
 , fullName varchar(256)
-, characterEncoding varchar(128) NOT NULL
-, cipherTransformation varchar(128) NOT NULL
-, aesEncryptionAlgorithm varchar(128) NOT NULL
+, publicKey varchar(128) NOT NULL
 , primary key (id));
 
 /*
- * Create a template table for accounts
+ * Create a template table for Objects
  * with an username and password
  */
-CREATE table accounts_template(ID int auto_increment
+CREATE table passwordaccounts_template(ID int auto_increment
 , url varchar(2048)
 , name varchar(256) NOT NULL
 , username varchar(256) NOT NULL
 , encryptedPassword varchar(256)
-, encryptionKey varchar(256)
 , notes varchar(10240)
+, characterEncoding varchar(128) NOT NULL
+, cipherTransformation varchar(128) NOT NULL
+, aesEncryptionAlgorithm varchar(128) NOT NULL
 , primary key (id));
 
 /*********************************************************************************************************/
@@ -56,28 +56,24 @@ CREATE table accounts_template(ID int auto_increment
  *
  * Parameter paramUserName			Username for the new account
  * Parameter paramFullName			Full name of the new account user (optional)
- * Parameter paramCharacterEncoding
- * Parameter paramCipherTransformation
- * Parameter paramAESEncryptionAlgorithm
+ * Parameter paramPublicKey			Key used for encryption
  */
 DELIMITER //
 CREATE DEFINER=`jdbcopal`@`localhost`
-PROCEDURE `add_new_opaluser` (In paramUserName varchar(256), In paramFullName varchar(256), In paramCharacterEncoding varchar(128),
-								In paramCipherTransformation varchar(128), In paramAESEncryptionAlgorithm varchar(128))
+PROCEDURE `add_new_opaluser` (In paramUserName varchar(256), In paramFullName varchar(256), In paramPublicKey varchar(128))
 BEGIN
 	If not exists (Select 1 FROM information_schema.TABLES
 	WHERE table_schema=DATABASE()
 	AND table_name=CONCAT(paramUserName,'_accounts') )
 	Then
         
-        SET @sql = CONCAT('CREATE TABLE ', CONCAT(paramUserName,'_accounts'),' LIKE accounts_template');
+        SET @sql = CONCAT('CREATE TABLE ', CONCAT(paramUserName,'_accounts'),' LIKE passwordaccounts_template');
 		PREPARE s FROM @sql;
 		EXECUTE s;
 		DEALLOCATE PREPARE s;
 
-		SET @sql = CONCAT('INSERT INTO opalUsers (username, fullName, characterEncoding, cipherTransformation, aesEncryptionAlgorithm) VALUES (',"'",paramUserName,
-							"'",', ',"'",paramFullName,"'",', ',"'",paramCharacterEncoding,"'",', ',"'",paramCipherTransformation,"'",', ',"'",
-                            paramAESEncryptionAlgorithm,"'",')');
+		SET @sql = CONCAT('INSERT INTO opalUsers (username, fullName, publicKey) VALUES (',"'",paramUserName,
+							"'",', ',"'",paramFullName,"'",', ',"'",paramPublicKey,"'",')');
 		PREPARE s FROM @sql;
 		EXECUTE s;
 		DEALLOCATE PREPARE s;
@@ -126,21 +122,24 @@ DELIMITER ;
  * Parameter paramURL						URL of the account
  * Parameter paramAccountUsernmame			Username of the account being added
  * Parameter paramEncryptedPassword			Password after encryption
- * Parameter paramEncryptionKey				Randomly generated key for encrypting and decrypting account password
  * Parameter paramNotes						Any notes to store with the account
+ * Parameter paramCharacterEncoding
+ * Parameter paramCipherTransformation
+ * Parameter paramAesEncryptionAlgorithm
  */
 DELIMITER //
 CREATE DEFINER=`jdbcopal`@`localhost`
 PROCEDURE `add_account` (In paramName varchar(256), In paramOpalName varchar(256), In paramURL varchar(2048), In paramAccountUsername varchar(256),
-							In paramEncryptedPassword varchar(256), In paramEncryptionKey varchar(256), In paramNotes varchar(10240))
+							In paramEncryptedPassword varchar(256), In paramNotes varchar(10240), In paramCharacterEncoding varchar(128),
+                            In paramCipherTransformation varchar(128), In paramAesEncryptionAlgorithm varchar(128))
 BEGIN
 	If exists (Select 1 FROM information_schema.TABLES
 	WHERE table_schema=DATABASE()
 	AND table_name=CONCAT(paramOpalName,'_accounts') )
 	Then
-		SET @sql = CONCAT('INSERT INTO ', CONCAT(paramOpalName,'_accounts'),' (url, name, username, encryptedPassword, encryptionKey, notes) VALUES(',"'",
-							paramURL,"'",', ',"'",paramName,"'",', ',"'",paramAccountUsername,"'",', ',"'",paramEncryptedPassword,"'",', ',"'",paramEncryptionKey,
-                            "'",', ',"'",paramNotes,"'",')');
+		SET @sql = CONCAT('INSERT INTO ', CONCAT(paramOpalName,'_accounts'),' (name, url, username, encryptedPassword, notes, characterEncoding, cipherTransformation, aesEncryptionAlgorithm) VALUES(',"'",
+							paramName,"'",', ',"'",paramURL,"'",', ',"'",paramAccountUsername,"'",', ',"'",paramEncryptedPassword,"'",', ',"'",paramNotes,"'",', ',"'",
+                            paramCharacterEncoding,"'",', ',"'",paramCipherTransformation,"'",', ',"'",paramAesEncryptionAlgorithm,"'",')');
 		PREPARE s FROM @sql;
         EXECUTE s;
         DEALLOCATE PREPARE s;
@@ -153,8 +152,8 @@ DELIMITER ;
 /*
  * Removes an account from the password manager.
  *
- * Parameter paramOpalName		Opal username this account is stored under
- * Parameter paramRemoveAccount	Name of the account being removed
+ * Parameter paramRemoveAccount		Name of the account being removed
+ * Parameter paramOpalName			Opal username this account is stored under
  */
 DELIMITER //
 CREATE DEFINER=`jdbcopal`@`localhost`
