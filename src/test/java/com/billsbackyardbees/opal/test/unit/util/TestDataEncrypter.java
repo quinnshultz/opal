98778a0b5abbd999c206e19010eae3cf7f0b92ae
Quinn Shultz <vanoxite@gmail.com>
2019-01-18 18:28:39 -0700

    Added test suite for DataEncrypter class

---
+++ src/test/java/com/billsbackyardbees/opal/test/unit/util/TestDataEncrypter.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2019 Quinn Shultz <vanoxite@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.billsbackyardbees.opal.test.unit.util;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import com.billsbackyardbees.opal.util.DataEncrypter;
+
+/**
+ * Tests the DataEncrypter class
+ * @author Quinn Shultz
+ */
+public class TestDataEncrypter extends TestCase {
+
+	private DataEncrypter secretDecoderRing;
+	
+	/**
+	 * Constructs a new DataEncrypter Object
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		secretDecoderRing = new DataEncrypter();
+	}
+	
+	/**
+	 * Tests that a new DataEncrypter Object is constructed
+	 */
+	@Test
+	public void testConstructor() {
+		assertNotNull(secretDecoderRing);
+	}
+
+}
